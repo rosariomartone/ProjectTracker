f4e6ed037b81e75a30e872921d440652396a7c06
rosar <rosar@DESKTOP-EPP5N1F>
2017-04-07 19:31:37 +0100

    Registration

---
+++ ProjectTrackerAPI/ProjectTrackerAPI/Controllers/RegistrationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+using ProjectTrackerBusinessLogic;
+
+namespace ProjectTrackerAPI.Controllers
+{
+    public class RegistrationController : ApiController
+    {
+        [HttpPut]
+        [Route("api/data/registration")]
+        public IHttpActionResult GetOpportunities()
+        {
+            return Ok(OpportunityLogic.GetOpportunities());
+        }
+    }
+}
